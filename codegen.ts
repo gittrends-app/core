2e3a44ab78c2018aa6cf50e37b15cffbeae661a3
Hudson Silva Borges <hudsonsilbor@gmail.com>
2025-12-18 23:16:35 -0300

    refactor: wIP: remove octokit-schema devido a problemas na atualização

---
+++ codegen.ts
@@ -0,0 +1,14 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+ 
+const config: CodegenConfig = {
+  schema: 'https://docs.github.com/public/fpt/schema.docs.graphql',
+  documents: ['src/**/*.ts'],
+  ignoreNoDocuments: true,
+  generates: {
+    './src/services/github/schema.d.ts': {
+      plugins: ['typescript'],
+    }
+  }
+}
+ 
+export default config
